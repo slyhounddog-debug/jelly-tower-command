261983cde7f29409e36e997303fa5979d33ffe3e
Slyhounddog <slyhounddog@example.com>
2026-01-13 18:50:12 +0000

    fixed loot drop placement and coin value

--- drop.js
+++ drop.js
@@ -14,7 +14,7 @@ export default class Drop {
         this.gravity = 0.3;
         this.life = 1200; // Longer life
         this.width = 70; // Changed from 40 to 70 (75% bigger)
-        this.coinValue = (type === 'lucky_coin') ? 100 : (type === 'coin' ? 250000 : 0);
+        this.coinValue = (type === 'lucky_coin') ? 100 : (type === 'coin' ? 25 : 0);
         this.xpValue = (type === 'xp_orb') ? value : 0;
         this.rotation = 0;
         this.rotationSpeed = this.vx * 0.1;
@@ -40,7 +40,7 @@ export default class Drop {
 
         if (this.x < 0) { this.x = 0; this.vx *= -0.8; }
         if (this.x > this.game.width - this.width) { this.x = this.game.width - this.width; this.vx *= -0.8; }
-        if (this.y > this.game.height - 190 - this.width / 2) { this.y = this.game.height - 190 - this.width / 2; this.vy *= -0.3; }
+        if (this.y > this.game.height - 90 - this.width / 2) { this.y = this.game.height - 90 - this.width / 2; this.vy *= -0.3; }
 
         if (Math.abs(this.game.player.x - this.x) < this.game.player.pickupRange && Math.abs(this.game.player.y - this.y) < this.game.player.pickupRange) {
             if (this.type === 'coin') {
